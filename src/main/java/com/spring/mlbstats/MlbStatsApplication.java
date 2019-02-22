c9e7a805165d03f066305b512934ea1621dfb55e
Teja <phankanmp@gmail.com>
2019-02-21 23:16:16 -0800

    Initial commit

---
+++ src/main/java/com/spring/mlbstats/MlbStatsApplication.java
@@ -0,0 +1,13 @@
+package com.spring.mlbstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlbStatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlbStatsApplication.class, args);
+    }
+
+}
